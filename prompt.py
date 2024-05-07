ffecd32e0dfed4906081cf0be230cbf9dd9dbf5f
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2024-05-07 21:13:43 +0800

    Use a Retrieval QA chain

--- prompt.py
+++ prompt.py
@@ -1,12 +1,26 @@
 from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
+chat = ChatOpenAI(verbose=True)
 embeddings = OpenAIEmbeddings()
 db = Chroma(
     persist_directory="emb",
     embedding_function=embeddings,
 )
+
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever=retriever,
+    chain_type="stuff",
+)
+
+result = chain.run("What is an interesting fact about the english language?")
+
+print(result)
